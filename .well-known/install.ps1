b9b83c9378d68dcd80427d4e7acfaf9b290a4822
Sean Severance Williams <mail@seansev.com>
2025-11-22 11:27:51 -0500

    Track tooling

---
+++ .well-known/install.ps1
@@ -0,0 +1,1 @@
+gpg-wks-client install-key -v D2A8253ADBF5F8AC70ED47A1DE3EF16F0FC606B2 mail@seansev.com
---
+++ .well-known/install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gpg-wks-client install-key -v D2A8253ADBF5F8AC70ED47A1DE3EF16F0FC606B2 mail@seansev.com
